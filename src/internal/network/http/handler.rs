bff24db1d89abcd0718bfcfd9b142bf95bcdb4bd
kien Nguyen <nkien0204@gmail.com>
2023-04-09 22:29:55 +0700

    not done yet

--- src/internal/network/http/handler.rs
+++ src/internal/network/http/handler.rs
@@ -1,4 +1,4 @@
-use reqwest::blocking::Client;
+use reqwest::{blocking::Client, header::{HeaderMap, USER_AGENT}};
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, time::Duration};
 
@@ -22,15 +22,22 @@ impl GithubApiHandler {
     pub fn new(url: String) -> Self {
         Self { url }
     }
+
+    fn set_headers() -> HeaderMap {
+        let headers = HashMap::new();
+        headers.insert(USER_AGENT, HeaderValue::from)
+    }
+
     pub fn create_gist(&self, body: CreateGistBody) -> Result<String, Box<dyn std::error::Error>> {
         let serial_body = serde_json::to_string(&body)?;
         let client = Client::builder().timeout(Duration::from_secs(10)).build()?;
         let response = client
             .post(self.url.clone())
             .header("Content-Type", "application/json")
+            .headers()   "User-Agent", "lcs",
             .body(serial_body)
             .send()?;
-        // println!("{}", response.text()?);
+        println!("status code: {}", response.status());
         Ok(response.text()?)
     }
 }
